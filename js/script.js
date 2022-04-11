b34274ed1580ef0b00db974d53e5902e8d5de63e
Nathan Araujo <99274283+nathan-araujo@users.noreply.github.com>
2022-04-11 15:05:25 +0000

    added comments

--- js/script.js
+++ js/script.js
@@ -12,11 +12,12 @@
 function calcVolume () {
   // get user input
   let side_a = parseFloat(document.getElementById('side-a').value)
+	let side_b = parseFloat(document.getElementById('side-b').value)
 	let height = parseFloat(document.getElementById('height').value)
 
 
   // calculate the volume
-  let volume = (1/3) * Math.pow(side_a,2) * height 
+  let volume = (1/6) * side_b * side_a * height 
 
   // display the results
   document.getElementById('volume').innerHTML = "The volume is " + volume.toFixed(2) + "m<sup>3</sup>"
